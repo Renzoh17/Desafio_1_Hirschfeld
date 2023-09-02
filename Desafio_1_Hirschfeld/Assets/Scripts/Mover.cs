baa5d4632be649bb8cc86c7daff195df67914a9c
Renzoh17 <hirschfeldrenzo@gmail.com>
2023-09-02 19:09:51 -0300

    Desafio 2
    Animacion de Reposo, Salto y Correr

--- Desafio_1_Hirschfeld/Assets/Scripts/Mover.cs
+++ Desafio_1_Hirschfeld/Assets/Scripts/Mover.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    
     // Variables a configurar desde el editor
     [Header("Configuracion")]
     [SerializeField] float velocidad = 5f;
@@ -15,11 +14,20 @@ public class Mover : MonoBehaviour
 
     // Variable para referenciar otro componente del objeto
     private Rigidbody2D miRigidbody2D;
+    private Animator miAnimator;
+    private SpriteRenderer miSprite;
+    private PolygonCollider2D miCollider2D;
+
+    private int saltarMask;
 
     // Codigo ejecutado cuando el objeto se activa en el nivel
     private void OnEnable()
     {
         miRigidbody2D = GetComponent<Rigidbody2D>();
+        miAnimator = GetComponent<Animator>();
+        miSprite = GetComponent<SpriteRenderer>();
+        miCollider2D = GetComponent<PolygonCollider2D>();
+        saltarMask = LayerMask.GetMask("Pisos", "Plataformas");
     }
 
     // Codigo ejecutado en cada frame del juego (Intervalo variable)
@@ -27,10 +35,23 @@ public class Mover : MonoBehaviour
     {
         moverHorizontal = Input.GetAxis("Horizontal");
         direccion = new Vector2(moverHorizontal, 0f);
+       
+        int velocidadX = (int)miRigidbody2D.velocity.x;
+        miSprite.flipX = velocidadX < 0;
+        miAnimator.SetInteger("Velocidad", velocidadX);
+
+        miAnimator.SetBool("EnAire", !EnContactoConPlataforma());
+        
     }
+
     private void FixedUpdate()
     {
         miRigidbody2D.AddForce(direccion * velocidad);
+    }
+
+    private bool EnContactoConPlataforma()
+    {
+        return miCollider2D.IsTouchingLayers(saltarMask);
     }
 }
 
